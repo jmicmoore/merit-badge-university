61dfe2474282f87d5b59e4cd2527b6212a082633
Jerry Moore <jmoor6@monsanto.com>
2017-08-29 20:51:38 -0500

    - added ejs to bundle the app

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./src/client'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'src/server');
+
+app.use(express.static('./dist'));
+
+
+app.get('*', function(req, res) {
+    res.render('index');
+});
 
 app.listen(3000, function () {
     console.log('Server listening on port 3000!')
