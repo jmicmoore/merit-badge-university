fecbe96e1675e9d80f18106e3b47b83d9cfaffbb
Jerry Moore <jmoor6@monsanto.com>
2017-08-29 21:21:49 -0500

    - adding a React form to play with

---
+++ src/client/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: ''
+        };
+
+        this.handleFirstNameChange = this.handleFirstNameChange.bind(this);
+        this.handleLastNameChange = this.handleLastNameChange.bind(this);
+    };
+
+    handleFirstNameChange(event){
+        this.setState({firstName: event.target.value});
+    };
+
+    handleLastNameChange(event){
+        this.setState({lastName: event.target.value});
+    };
+
+    render(){
+        return (
+            <form>
+                <label>
+                    First Name:
+                    <input type="text" value={this.state.firstName} onChange={this.handleFirstNameChange} />
+                </label>
+                <label>
+                    Last Name:
+                    <input type="text" value={this.state.lastName} onChange={this.handleLastNameChange} />
+                </label>
+            </form>
+        );
+    };
+};
+
+export default App;
