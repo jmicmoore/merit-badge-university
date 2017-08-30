54154fe87532e0e7bce3a0861c53ee678007ce9b
Jerry Moore <jmoor6@monsanto.com>
2017-08-29 21:30:43 -0500

    - added hotloading

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('content')
-);
+renderWithHotReload(App);
+
+module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    renderWithHotReload(NextApp);
+});
+
+function renderWithHotReload(Component) {
+    ReactDOM.render(
+        <AppContainer>
+            <Component/>
+        </AppContainer>,
+        document.getElementById('content')
+    );
+};
--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var app = express();
-
+var webpack = require('webpack');
+var webpackConfig = require('../../webpack.config');
+var compiler = webpack(webpackConfig);
 
 app.set('view engine', 'ejs');
 app.set('views', 'src/server');
 
 app.use(express.static('./dist'));
 
+const webpackDevMiddleware = require('webpack-dev-middleware');
+app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: '/',
+    stats: {
+        colors: true
+    },
+    historyApiFallback: true
+}));
+
+const webpackHotMiddleware = require('webpack-hot-middleware');
+app.use(webpackHotMiddleware(compiler));
 
 app.get('*', function(req, res) {
     res.render('index');
@@ -15,3 +29,8 @@ app.get('*', function(req, res) {
 app.listen(3000, function () {
     console.log('Server listening on port 3000!')
 });
+
+
+
+
+
